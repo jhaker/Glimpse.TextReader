391c95d475e0c243cfd8c50c554e47c991f475b6
UHAKEJO <jonathan.haker@pearson.com>
2013-05-13 08:09:58 -0500

    added git ignore

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,14 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Jhaker.Glimpse.TextReader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Glimpse-TextReader")]
+[assembly: AssemblyDescription("Plugin for Glimpse that reads any text file")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Jhaker.Glimpse.TextReader")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany("Jonathan Haker")]
+[assembly: AssemblyProduct("Glimpse-TextReader")]
+[assembly: AssemblyCopyright("Copyright ©  2013 Jonathan Haker")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
